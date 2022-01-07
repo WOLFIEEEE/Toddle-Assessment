df355930e46dc730649c2a889596d69712fbdfaa
WOLFIEEEE <kgpkhushwant1@gmail.com>
2022-01-07 16:06:08 +0530

    minor changes

--- routes/image/api_desc.js
+++ routes/image/api_desc.js
@@ -12,7 +12,7 @@ exports.resize = async(req, res, next) => {
             try {
                 sharp(buffer).resize(50, 50).jpeg({ quality: 50 }).toFile('./img/image_thumb.jpeg');
                 res.status(200)
-                    .send(`Original Image ${req.body.url}  <br> Image resized https://toddlef322.herokuapp.com/image_thumb.jpeg`);
+                    .send(`Original Image :- ${req.body.url}  </br> Image resized https://toddlef322.herokuapp.com/image_thumb.jpeg`);
 
             } catch (error) {
                 console.log(error);
